dad5e1572fd2f5f83f5e9c828be696371192d198
Maxwell Flitton <maxwellflitton@gmail.com>
2018-06-25 20:27:40 +0100

    basic folder structure added

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deployml_pkg",
+    version="0.0.1",
+    author="Maxwell Flitton, Rmain Belia",
+    author_email="maxwellflitton@gmail.com",
+    description="Easy training and deployment of machine learning algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deploy-ml/deploy-ml",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
