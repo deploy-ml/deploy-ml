b10df241a2a972b7b801b329399356d7fd39967b
Romain Belia <belia.bourgeois.romain@gmail.com>
2018-06-28 19:59:42 +0100

    Import for packages

---
+++ deployml/__init__.py
@@ -0,0 +1,5 @@
+import sklearn
+
+import keras
+
+import tensorflow
---
+++ deployml/sklearn/__init__.py
@@ -0,0 +1,7 @@
+"""Sk-learn module."""
+
+from deployml.sklearn.decision_tree import DecisionTree
+
+from deployml.sklearn.logistic_regression import LogisticRegressionBase
+
+from deployml.sklearn.neural_network import NeuralNetworkBase
