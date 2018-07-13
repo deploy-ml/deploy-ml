b6327cd3d43b5e18fbe962bed999f8608b19fa0c
Maxwell Flitton <maxwellflitton@gmail.com>
2018-07-13 18:31:33 +0100

    Keras metrics added

--- deployml/keras/__init__.py
+++ deployml/keras/__init__.py
@@ -1,4 +1,6 @@
 
 from deployml.keras.models.neural_network import NeuralNetworkBase
 
+from deployml.keras.models.recurrent_neural import RecurrentNeuralNetwork
+
 from deployml.keras.models.import_base import ImportBase
