a325a3992a13f277f02062a474957bfc415e28fc
Romain Belia <belia.bourgeois.romain@gmail.com>
2018-07-23 17:35:39 +0100

    Bump version to 0.0.2

--- deployml/keras/__init__.py
+++ deployml/keras/__init__.py
@@ -1,6 +1,8 @@
 
-from deployml.keras.models.neural_network import NeuralNetworkBase
+from deployml.keras.models.neural_network import NeuralNetworkBase  # noqa
 
-from deployml.keras.models.recurrent_neural import RecurrentNeuralNetwork
+# from deployml.keras.models.recurrent_neural import RecurrentNeuralNetwork  # noqa
 
-from deployml.keras.models.import_base import ImportBase
+from deployml.keras.models.import_base import ImportBase  # noqa
+
+from deployml.keras.version import VERSION  # noqa
